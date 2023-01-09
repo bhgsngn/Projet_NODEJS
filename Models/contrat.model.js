6c421404211509b2599b8d5f6837ee17f433c3b5
bhgsngn <94994243+bhgsngn@users.noreply.github.com>
2023-01-09 18:34:21 +0100

    Add files via upload

---
+++ Models/contrat.model.js
@@ -0,0 +1,19 @@
+//Le modèle de base de données "Contrat" a des champs de Num,description Contrat.
+
+
+module.exports = (sequelize, Sequelize) => {
+  const Contrat = sequelize.define('contrat', {
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    num: {
+      type: Sequelize.INT,
+      primaryKey: true
+    },
+  });
+
+  return Contrat;
+};
+
+
